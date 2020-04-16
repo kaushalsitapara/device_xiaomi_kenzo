3fdaf5286c6235c0bda5e940ad434b9483fcf0ac
Kaushal Sitapara <ksitapara8@gmail.com>
2020-04-16 12:25:01 +0000

    ion bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_kenzo.mk
+    $(LOCAL_DIR)/ion_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_kenzo-userdebug \
-    bliss_kenzo-eng
+    ion_kenzo-userdebug \
+    ion_kenzo-eng
